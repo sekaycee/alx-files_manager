7a016f343bdb4e2965a4143543c7cd4fa14b8119
Kay Cee <se.kaycee@gmail.com>
2023-09-05 20:27:28 +0100

    feat: add first api

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import AppController from '../controllers/AppController';
+
+export default function Routes(app) {
+  const router = express.Router();
+  app.use('/', router);
+
+  router.get('/status', (req, res) => {
+    AppController.getStatus(req, res);
+  });
+
+  router.get('/stats', (req, res) => {
+    AppController.getStats(req, res);
+  });
+}
